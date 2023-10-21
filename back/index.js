645720bec617c6658d199bf8899982933b301e77
rslugo9709 <ras.inte@gmail.com>
2023-10-20 20:35:51 -0500

    testing

--- back/index.js
+++ back/index.js
@@ -3,11 +3,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 const port = process.env.PORT || 3004;
-
+console.log(process.env.PORT);
 
 try {
   conn().then(() =>{
-    server.listen(port, () => {
+    server.listen(port,"0.0.0.0", () => {
       console.log(`Server listening on port ${port}`);
     });
   });
